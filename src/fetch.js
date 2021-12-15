d130d42759d47bd9f92a28ed032d0ed764fef197
steve <steve.huckle@gmail.com>
2021-12-15 20:41:24 +0000

    cleanup

--- src/fetch.js
+++ src/fetch.js
@@ -5,7 +5,6 @@ export const fetchData = async (props) => {
 
       const response = options ? await fetch(url, options) : await fetch(url)
       const data = await response.json()
-      console.log('fetched data', data)
       if ( cb ) cb(data)
 
   } catch (error) {
